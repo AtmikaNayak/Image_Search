8ecc9f56773d60ccbb948986dc6ae4c00702bfa0
AtmikaNayak <atmikanayak021206@gmail.com>
2026-06-19 23:01:31 +0530

    -

--- api/app.js
+++ api/app.js
@@ -42,6 +42,4 @@ app.post('/search', async(req,res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Running on port ${PORT}`);
-});
+module.exports = app;
